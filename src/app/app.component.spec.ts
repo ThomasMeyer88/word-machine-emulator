1a2476c7a5f1279f92c00e59d8577b5c8adf2c3f
Thomas Meyer <thomasmmeyer88@gmail.com>
2021-07-27 12:44:32 -0500

    removed pointless test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('word-machine-emulator');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('word-machine-emulator app is running!');
-  });
 });
